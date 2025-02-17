c16efce2ca3dcf4efb3532d3a62324487b8fbefb
Zeca Monteiro <zecamonteiro991@gmail.com>
2025-02-17 13:44:53 -0300

    Inserindo projetos de anatomia de classes e tipos de variaveis

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,18 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+     
+        System.out.println("Olá, mundo!");
+
+        final String BR = "Brasil";
+
+        boolean contemOpcao = false;
+
+        String primeiroNome = "Zeca";
+        String segundoNome = "Monteiro";
+
+        String nomeCompleto = primeiroNome.concat(" ").concat(segundoNome);
+
+        System.out.println(nomeCompleto);
+        
+    }    
+}
